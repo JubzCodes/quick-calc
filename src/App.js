9ade509d7655c546f1af81914fa01066cf7c7f13
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-08-14 19:20:10 -0400

    button layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import './app.css';
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="grid">
+      <div className="output">
+        <div className="previous"></div>
+        <div className="current"></div>
+      </div>
+      <button>A/C</button>
+      <button>Del</button>
+      <button>div</button>
+      <button>9</button>
+      <button>8</button>
+      <button>7</button>
+      <button>mul</button>
+      <button>6</button>
+      <button>5</button>
+      <button>4</button>
+      <button>plu</button>
+      <button>3</button>
+      <button>2</button>
+      <button>1</button>
+      <button>min</button>
+      <button>.</button>
+      <button>0</button>
+      <button>=</button>
     </div>
   );
 }
