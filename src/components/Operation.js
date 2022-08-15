4bc7c7f410541c2e72c5cd8c26b9bc2cca2adca2
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-08-15 18:44:04 -0400

    create operation button & icon filter function

---
+++ src/components/Operation.js
@@ -0,0 +1,39 @@
+import { ACTIONS } from "../App";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlus,
+  faXmark,
+  faDivide,
+  faMinus,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function OperatorButton({ dispatch, operator, icons }) {
+
+  const iconPicker = (icons) => {
+
+    switch (icons) {
+
+      case icons.DIVIDE:
+        return <FontAwesomeIcon icon={faDivide}></FontAwesomeIcon>;
+
+      case icons.MULTIPLY:
+        return <FontAwesomeIcon icon={faXmark}></FontAwesomeIcon>;
+
+      case icons.PLUS:
+        return <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>;
+
+      case icons.MINUS:
+        return <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>;
+
+    }
+
+  }
+
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATOR, payload: { operator } })}
+    >
+      {iconPicker(icons)}
+    </button>
+  );
+}
